648f0e6ab40d8e7e7529e936aaaa1d4384df0003
Benjamin VIELLARD <gbicou@gmail.com>
2024-12-23 14:55:19 +0100

    chore: use @vue/eslint-config-typescript

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,25 @@
-/* eslint-env node */
-
 import js from '@eslint/js'
 import ts from 'typescript-eslint'
 import vue from 'eslint-plugin-vue'
 import stylistic from '@stylistic/eslint-plugin'
 import tailwind from 'eslint-plugin-tailwindcss'
+import vueTsEslintConfig from '@vue/eslint-config-typescript'
 
 export default ts.config(
   {
     ignores: ['dist/'],
   },
+
   // javascript
   js.configs.recommended,
-  {
-    rules: {
-      // "no-unused-vars": "off",
-      // "no-undef": "off",
-    },
-  },
 
   // typescript
   ts.configs.strict,
   ts.configs.stylistic,
-  {
-    rules: {
-      // "@typescript-eslint/no-unused-vars": "warn",
-      // "@typescript-eslint/no-explicit-any": "warn",
-    },
-  },
 
   // vue
   vue.configs['flat/recommended'],
-  {
-    files: ['*.vue', '**/*.vue'],
-    languageOptions: {
-      parserOptions: {
-        parser: ts.parser,
-      },
-    },
-  },
+  vueTsEslintConfig(),
 
   // tailwind
   tailwind.configs['flat/recommended'],
