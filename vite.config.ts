9778a8383848d0d4a1ccc8685e596f6ca9356d05
Benjamin VIELLARD <gbicou@gmail.com>
2025-01-23 12:32:07 +0100

    chore: suspend tailwind eslint plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { join } from 'node:path'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
