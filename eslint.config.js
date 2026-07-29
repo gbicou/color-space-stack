e6ce6da4f306c079a777a31b783ec5d27393df87
Benjamin VIELLARD <gbicou@gmail.com>
2026-07-29 16:16:52 +0200

    chore(eslint): re-enable tailwind plugin and update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,16 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import { withVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import pluginVitest from '@vitest/eslint-plugin'
 import stylistic from '@stylistic/eslint-plugin'
+import tailwind from 'eslint-plugin-tailwindcss'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
 // configureVueProject({ scriptLangs: ['ts', 'tsx'] })
 // More info at https://github.com/vuejs/eslint-config-typescript/#advanced-setup
 
-export default defineConfigWithVueTs(
+export default withVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{vue,ts,mts,tsx}'],
@@ -17,15 +18,22 @@ export default defineConfigWithVueTs(
 
   globalIgnores(['**/dist/**']),
 
-  ...pluginVue.configs['flat/recommended'],
+  pluginVue.configs['flat/recommended'],
   vueTsConfigs.strict,
   vueTsConfigs.stylistic,
 
   // stylistic
   stylistic.configs['recommended'],
 
-  // tailwind removed until plugin supports v4
-  // tailwind.configs['flat/recommended']
+  {
+    files: ['**/*.{vue,ts,mts,tsx}'],
+    extends: [tailwind.configs.recommended],
+    settings: {
+      tailwindcss: {
+        cssConfigPath: './src/assets/main.css',
+      },
+    },
+  },
 
   {
     ...pluginVitest.configs.recommended,
